a1e4b2557d8b7bbe85fc75550a0920ad0f4f7183
github username <github email address>
2019-10-04 12:59:10 -0400

    added tests for booksreducer

---
+++ alexandria/src/reducers/booksReducer/booksReducer.js
@@ -0,0 +1,11 @@
+import { actionTypes } from "../../actionTypes";
+
+export default (defaultState=[], action) => {
+    switch(action.type){
+        case actionTypes.RETRIEVE_MY_BOOKS:
+            return [...action.payload]
+        default: 
+            return defaultState
+    }
+    return defaultState
+}
