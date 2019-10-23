ddfe6ceeb844fcc561a02e2d1b4d88d8652c625c
github username <github email address>
2019-10-23 13:34:01 -0400

    add book mutation set up in graphql

--- server/schemas/mutations.js
+++ server/schemas/mutations.js
@@ -1,24 +1,62 @@
 const graphql = require("graphql");
 // const db = require("../pgAdaptor").db;
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean } = graphql;
-const { UserType } = require("./types");
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean, GraphQLNonNull } = graphql;
+const { UserType, UserBookType } = require("./types");
+const { sequelize } = require('../models/index')
+const { user, book, userbook, author } = sequelize.models
+
+// http://books.google.com/books/content?id=-ON2PwAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api
 
 const RootMutation = new GraphQLObjectType({
     name: "RootMutationType",
     type: "Mutation",
     fields: {
-        addUser: {
-            type: UserType,
+        // addUser: {
+        //     type: UserType,
+        //     args: {
+        //         username: { type: new GraphQLNonNull(GraphQLString) },
+        //         email: { type: new GraphQLNonNull(GraphQLString) }
+        //     },
+        //     resolve(parentValue, args) {
+        //         // const query = `INSERT INTO user(name, email) VALUES ($1, $2) RETURNING name`;
+        //         // const values = [
+        //         //     args.name,
+        //         //     args.email
+        //         // ];
+        //     }
+        // },
+        addBook: {
+            type: UserBookType,
             args: {
-                name: { type: GraphQLString },
-                email: { type: GraphQLString }
+                userId: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                author: { type: new GraphQLNonNull(GraphQLString) },
+                thumbnail: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                // const query = `INSERT INTO user(name, email) VALUES ($1, $2) RETURNING name`;
-                // const values = [
-                //     args.name,
-                //     args.email
-                // ];
+                author.findOrCreate({
+                    where: {
+                        name: args.author
+                    }
+                })
+                    .then((author, created)=>{
+                        const authorId = author[0].dataValues.id
+                        return book.findOrCreate({
+                            where: {
+                                title: args.title,
+                                authorId: authorId
+                            }
+                        })
+                    .then((book, created) => {
+                        const bookId = book[0].dataValues.id
+                        userbook.findOrCreate({
+                            where: {
+                                userId: args.userId,
+                                bookId: bookId
+                            }
+                        })
+                    })
+                })
             }
         }
     }
