a654d57379f98d4a9bad785709199202de9e1dc2
github username <github email address>
2019-10-29 10:46:26 -0400

    eslint and nav links

---
+++ alexandria/src/bookshow/BookShow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BookShow = (props) => {
+    
+    const { id } = props.match.params
+
+
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
+
+export default BookShow
